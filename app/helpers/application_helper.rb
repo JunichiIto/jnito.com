d9217d204f84e94622518e3d02bfbeabd84e8137
Junichi Ito <jit@sonicgarden.jp>
2017-09-24 14:19:03 +0900

    Set favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def gravatar_url
+  def gravatar_url(size = 165)
     gravatar_id = Digest::MD5::hexdigest('me@jnito.com')
-    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=165"
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
   end
 end
