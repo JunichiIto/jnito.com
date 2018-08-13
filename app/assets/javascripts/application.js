ac36adbe8b60b8a13104e9e927935068cd484589
Junichi Ito <jit@sonicgarden.jp>
2018-08-13 17:51:29 +0900

    Remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery-1.10.2.min
 //= require jquery-migrate-1.2.1.min
 //= require jquery.flexslider
